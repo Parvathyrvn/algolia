622bcad5c3fe3e18f1dbd57adbdc913aec1b27c2
Parvathyrvn <parvathy@zoftsolutions.com>
2023-02-06 17:15:45 +0530

    Initial Commit

---
+++ src/main/java/com/demo/algolia/AlgoliaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.algolia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/algolia/AlgoliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.algolia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
